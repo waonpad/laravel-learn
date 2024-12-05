ccd85e2608934d83dddd2568f5e13cd168873f66
waonpad <waonpad@gmail.com>
2024-12-05 22:38:37 +0900

    不要アノテーション削除

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -17,8 +17,6 @@ final class ExampleTest extends TestCase
     /**
      * A basic test example.
      *
-     * @test
-     *
      * @covers ::index
      */
     public function testTheApplicationReturnsASuccessfulResponse(): void
