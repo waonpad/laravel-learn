391097b653ad252361841f702ff955550c77b35d
asai <asai@fenrir.co.jp>
2025-01-09 13:46:38 +0900

    ログインのリクエストとdtoを追加

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Post;
+
+use App\Http\DTO\Post\LoginDto;
+use App\Http\Requests\CustomFormRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class LoginRequest extends CustomFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, array<mixed>|string|ValidationRule>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function makeInput(): LoginDto
+    {
+        $validated = $this->validated();
+
+        return new LoginDto([
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+        ]);
+    }
+}
