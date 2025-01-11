4ad23582f1ff251057fe49783483b2b9dc33bd4e
waonpad <waonpad@gmail.com>
2025-01-11 17:33:56 +0900

    Revert "scrambleいれる"
    This reverts commit 34b5c5798dd0b9189a047de6d94a57fdd622e635.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Dedoc\Scramble\Scramble;
-use Dedoc\Scramble\Support\Generator\OpenApi;
-use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,14 +21,5 @@ class AppServiceProvider extends ServiceProvider
     {
         // dataプロパティでデータをラップしないようにする
         JsonResource::withoutWrapping();
-
-        Scramble::routes(function (Route $route) {
-            return true;
-        });
-        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
-            $openApi->secure(
-                SecurityScheme::http('bearer')
-            );
-        });
     }
 }
